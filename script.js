479fc43c48b252cb10fdce33f58a78ebd30e8bd0
Mikael Navbar <miguenavar@hotmail.com>
2024-03-08 20:46:34 -0600

    Actualizacion de index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const textarea = document.querySelector("text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textarea.value)
+    mensaje.value = textoEncriptado
+}
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e","enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+
+        }
+
+    }
+    return stringEncriptada
+
+}
